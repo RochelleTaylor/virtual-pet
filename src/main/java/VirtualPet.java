0638dc0b515f8743e55d8eb6bb6dde6ebf0b8095
Rochelle Taylor <rochelletaylor614@gmail.com>
2018-02-01 16:31:03 -0500

    Final commit

--- src/main/java/VirtualPet.java
+++ src/main/java/VirtualPet.java
@@ -1,16 +1,22 @@
 
 public class VirtualPet {
 
-	private int hunger= 30;
+	private int hunger = 30;
 
-	private int thirst= 30;
+	private int thirst = 30;
 
-	private int waste= 30;
+	private int waste = 30;
 
-	private int boredom= 30;
+	private int boredom = 30;
 
-	private int sickness=30;
+	private int sickness = 30;
+	boolean petIsAlive = true;
 
+	public VirtualPet() {
+
+	}
+
+	// constructors
 	public VirtualPet(int hunger, int thirst, int waste, int boredom, int sickness) {
 		this.hunger = hunger;
 		this.thirst = thirst;
@@ -37,5 +43,46 @@ public class VirtualPet {
 
 	public int getSickness() {
 		return sickness;
+	}
+
+	// Class behavior method
+	public void tick() {
+		this.hunger++;
+		this.boredom++;
+		this.waste++;
+		this.sickness++;
+		this.thirst++;
+
+		if (this.hunger > 100 || this.boredom > 100)
+			;
+		this.petIsAlive = false;
+
+		if (this.waste > 100 || this.sickness > 100)
+			;
+		this.petIsAlive = false;
+
+		if (this.thirst > 100)
+			;
+		this.petIsAlive = false;
+	}
+
+	public void feedPet() {
+		this.hunger -= 20;
+	}
+
+	public void waterPet() {
+		this.thirst -= 20;
+	}
+
+	public void playWithPet() {
+		this.boredom -= 20;
+	}
+
+	public void takePetToBathroom() {
+		this.waste -= 20;
+	}
+
+	public void healPet() {
+		this.sickness -= 20;
 	}
 }
--- src/main/java/VirtualPetApp.java
+++ src/main/java/VirtualPetApp.java
@@ -1,9 +1,63 @@
+import java.util.Scanner;
 
 public class VirtualPetApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in);
+
+		VirtualPet name = new VirtualPet();
+
+		System.out.println("What do you want to name your pet?");
+		String enterName = input.nextLine();
+
+		String optionsEntered = "";
+		do {
+			System.out.println(enterName);
+			System.out.println("Hunger:" + name.getHunger());
+			System.out.println("Thirst:" + name.getThirst());
+			System.out.println("Bathroom:" + name.getWaste());
+			System.out.println("Boredom:" + name.getBoredom());
+			System.out.println("Health:" + name.getSickness());
+			System.out.println();
+			System.out.println();
+			System.out.println("What do you wan to to do with your pet?");
+
+			System.out.println("Press 1 to feed " + enterName);
+			System.out.println("Press 2 to give " + enterName + " water");
+			System.out.println("Press 3 to take " + enterName + " to bathroom");
+			System.out.println("Press 4 to play with " + enterName);
+			System.out.println("Press 5 to to give " + enterName + " medicine");
+			optionsEntered = input.nextLine();
+
+			switch (optionsEntered) {
+			case "1": {
+				name.feedPet();
+				System.out.println("You fed " + enterName);
+				break;
+			}
+			case "2": {
+				name.waterPet();
+				System.out.println("You watered " + enterName);
+				break;
+			}
+			case "3": {
+				name.playWithPet();
+				System.out.println("You played with " + enterName);
+				break;
+			}
+			case "4": {
+				name.takePetToBathroom();
+				System.out.println("You took " + enterName + " to the bathroom");
+				break;
+			}
+			case "5": {
+				name.healPet();
+				System.out.println("You made " + enterName + " feel better!");
+				break;
+			}
+			}
+			name.tick();
+		} while (true);
 	}
-
 }
